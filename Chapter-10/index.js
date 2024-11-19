eec6c3d1a1c27e02065b0244905e2f9e3cb1641a
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-11-19 20:08:08 +0530

    Server Side Rendering

---
+++ Chapter-10/index.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const express = require('express');
+// const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const server = express();
+const path = require('path');
+const productRouter = require('./routes/product')
+const userRouter = require('./routes/user')
+console.log('env',process.env.DB_PASSWORD)
+
+
+// MongoDB connection
+mongoose.set('strictQuery', false);
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+  }
+}
+main();
+//Schema
+
+
+
+
+
+
+
+
+//bodyParser
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+// server.use(morgan('default'));
+server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
+server.use('/products',productRouter.router);
+server.use('/users',userRouter.router);
+server.use('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
+
+
+
+
+server.listen(process.env.PORT, () => {
+  console.log('server started');
+});
